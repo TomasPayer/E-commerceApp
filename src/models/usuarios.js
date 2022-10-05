aff1870693818fd857e51a121435679cb21efa2d
Tomas Payer <tomaspayer18@gmail.com>
2022-10-05 00:34:38 -0300

    changes

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { MONGO_URI } = require('../config/globals')
 
-mongoose.connect(MONGO_URI, {
+mongoose.connect("mongodb+srv://tomascoder:tomascoder@e-commerceapp.3hfrnuw.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser: true, 
     useUnifiedTopology: true
   }, () => console.log('Connected'))
